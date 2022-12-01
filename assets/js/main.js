70893db35e6d248094af1f288a631c66bd1a6c9f
Wellington Nogueira <wellingtoncw7@gmail.com>
2022-11-30 21:33:04 -0300

    add fetch para requisicao da pokeapi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log('teste')
+
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url) // retorna uma Promisse - Processamento assíncrono pois não tem a resposta de imediato
+    .then((response) => response.json()) // converte o resultado da Promisse para um formato json
+    .then((jsonBody) => console.log(jsonBody)) // recebe já transformado para json
+    .catch((error) => console.log(error)) // roda no caso de fracasso na requisição
+
