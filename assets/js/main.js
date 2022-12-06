344af050e3353dba528cebbeeeb557549729150f
Wellington Nogueira <wellingtoncw7@gmail.com>
2022-12-06 20:14:25 -0300

    add model pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,15 @@
 
-
 function convertPokemonToLi(pokemon) {
     return `
-            <li class="pokemon">
-                <span class="number">#001</span>
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
                 
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
+                       ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                     </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
                 </div>
             </li>
         `
@@ -20,8 +18,8 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById('pokemonList') // pega a lista ol do html
 
 pokeApi.getPokemons().then((pokemons = []) => { // recebe a listagem de pokemons já transformada do objeto json
-
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
 
 })
 
